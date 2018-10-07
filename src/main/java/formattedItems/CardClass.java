cf45a5bc6143987ed1538e1a64f8277bfe883737
calabain <afpsujek@gmail.com>
2018-10-07 17:06:59 -0500

    Added dummy backlog class

--- src/main/java/formattedItems/CardClass.java
+++ src/main/java/formattedItems/CardClass.java
@@ -1,5 +1,8 @@
 package formattedItems;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -10,6 +13,8 @@ public class CardClass {
     private VBox card = new VBox();
     private Text title = new Text();
     private Text content = new Text();
+    private final CheckBox checkBox = new CheckBox("Select to play card");
+    private final Text selected = new Text("Not selected");
     private String borderStyle = new String("-fx-padding: 10;" +
                                     "-fx-border-style: solid inside;" +
                                     "-fx-border-width: 5;" +
@@ -17,22 +22,37 @@ public class CardClass {
                                     "-fx-border-radius: 10;" +
                                     "-fx-border-color: black;");
 
-    public CardClass(String title, String content){
+    public CardClass(Text title, Text content){
         formatTitle(title);
         formatContent(content);
+        addCheckBoxListener(checkBox);
         card.getChildren().add(this.title);
         card.getChildren().add(this.content);
+        card.getChildren().add(this.checkBox);
+        card.getChildren().add(this.selected);
         card.setStyle(borderStyle);
     }
 
-    private void formatTitle(String str){
-        this.title.setText(str);
+    private void formatTitle(Text title){
+        this.title = title;
         this.title.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
     }
 
-    private void formatContent(String str){
-        this.content.setText(str);
+    private void formatContent(Text content){
+        this.content = content;
         this.content.setFont(Font.font("Verdana", FontWeight.NORMAL, 10));
+    }
+
+    private void addCheckBoxListener(final CheckBox checkBox){
+        checkBox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (checkBox.isSelected()) {
+                    selected.setText("Selected");
+                } else {
+                    selected.setText("Not Selected");
+                }
+            }
+        });
     }
 
     public VBox getCard(){
